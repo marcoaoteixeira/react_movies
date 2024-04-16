756a2dbeef0e6576d495cad69d87d0b9e12c971f
Marco Teixeira <marcoaoteixeira@gmail.com>
2024-04-16 11:19:44 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export default function App() {
+  return (
+    <Fragment>
+      <main className="h-screen m-8 flex gap-8">
+        <h1>It works</h1>
+      </main>
+    </Fragment>
+  );
+}
